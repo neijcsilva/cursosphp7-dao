de745544b72ae8beeb714be5e90d324080e55ee5
neijcsilva <62115463+neijcsilva@users.noreply.github.com>
2020-03-31 16:13:43 -0300

    Criados os métodos para listagem de dados

--- class/usuario.php
+++ class/usuario.php
@@ -20,7 +20,7 @@ class usuario{
     }
 
     public function setDeslogin($value){
-        $this->deslogino = $value;
+        $this->deslogin = $value;
     }
 
     public function getDessenha(){
@@ -58,6 +58,48 @@ class usuario{
             
         } 
         
+    }
+
+    public static function search($login){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", ARRAY(
+
+            ':SEARCH'=>"%".$login."%"
+        ));
+
+    }
+
+    public static function getList() {
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios ");    
+    }
+
+    public function login($login, $password) {
+        $sql = new Sql();
+        
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+        ));
+
+        if (count($results) > 0) {
+
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            
+        } else {
+
+            throw new Exception("Login ou Senha inválidos.");
+
+        }      
     }
 
     public function __toString() {
--- index.php
+++ index.php
@@ -3,15 +3,35 @@
 require_once("config.php");
 
 //$sql = new Sql();
-
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 //echo json_encode($usuarios);
 
-$root = new Usuario();
+//===================
+//Carrega um usuário
+//===================
+//$root = new Usuario();
+//$root->loadById(2);
+//echo $root;
 
-$root->loadById(2);
+//=============================
+//Carrega uma lista de usuários
+//=============================
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//============================================================================
+//Carrega uma lista de usuários buscando parcialmente ou totalmente pelo login
+//============================================================================
+//$search = Usuario::search("mar");
+//echo json_encode($search);
+
+//===========================================
+//Carrega um usuário usando o login e a senha
+//===========================================
+$usuario = new Usuario();
+$usuario->login("root", "1234567890");
+
+echo $usuario;
+
 
 ?>
